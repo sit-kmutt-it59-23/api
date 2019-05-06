b57d007e11c1c4eb57f6462d3e8dfdfa321c6f76
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2019-05-06 23:44:22 +0700

    [EDIT] Edit migration for user tables.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,15 +13,44 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        DB::beginTransaction();
+
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->unsignedBigInteger('id');
+            $table->string('username', 32);
+            $table->string('password', 255);
             $table->rememberToken();
-            $table->timestamps();
+            $table->datetime('deleted_at')->nullable();
+            $table->datetime('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->datetime('updated_at')
+                ->default(
+                    DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+                );
+
+            $table->primary('id');
         });
+
+        Schema::create('user_data', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->string('firstname', 64);
+            $table->string('middlename', 64)->nullable();
+            $table->string('lastname', 64);
+            $table->string('student_id', 11)->nullable();
+            $table->string('tel_no', 15)->nullable();
+            $table->rememberToken();
+            $table->datetime('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->datetime('updated_at')
+                ->default(
+                    DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+                );
+
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('restrict')->onDelete('cascade');
+            
+            $table->primary('user_id');
+        });
+
+        DB::commit();
     }
 
     /**
@@ -31,6 +60,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('user_data');
         Schema::dropIfExists('users');
     }
 }
