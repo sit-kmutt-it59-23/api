b6f33f3ffa063bd5b443b799641c9e1f8f655bf7
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2019-05-08 20:31:58 +0700

    [EDIT] Edit migration for organization tables.

--- database/migrations/2019_04_13_193704_create_organization_tables.php
+++ database/migrations/2019_04_13_193704_create_organization_tables.php
@@ -13,10 +13,66 @@ class CreateOrganizationTables extends Migration
      */
     public function up()
     {
-        Schema::create('organization_tables', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
+        DB::beginTransaction();
+
+        Schema::create('organization_types', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('name', 128);
         });
+
+        Schema::create('organization_categories', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('name', 128);
+        });
+
+        Schema::create('organizations', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->unsignedInteger('type_id');
+            $table->unsignedInteger('category_id')->nullable();
+            $table->string('name');
+            $table->string('name_en')->nullable();
+            $table->string('description')->nullable();
+            $table->string('slogan', 128)->nullable();
+            $table->string('logo_path')->nullable();
+            $table->datetime('expired_at')->nullable();
+            $table->datetime('deleted_at')->nullable();
+            $table->datetime('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->datetime('updated_at')
+                ->default(
+                    DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+                );
+            
+            $table->foreign('type_id')->references('id')->on('organization_types')
+                ->onUpdate('restrict')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('organization_categories')
+                ->onUpdate('restrict')->onDelete('cascade');
+        });
+
+        Schema::create('organization_user_levels', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('name', 128);
+        });
+
+        Schema::create('organization_user', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedInteger('organization_id');
+            $table->unsignedInteger('level_id');
+            $table->datetime('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->datetime('updated_at')
+                ->default(
+                    DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+                );
+            
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('restrict')->onDelete('cascade');
+            $table->foreign('organization_id')->references('id')->on('organizations')
+                ->onUpdate('restrict')->onDelete('cascade');
+            $table->foreign('level_id')->references('id')->on('organization_user_levels')
+                ->onUpdate('restrict')->onDelete('cascade');  
+        });
+
+        DB::commit();
     }
 
     /**
@@ -26,6 +82,10 @@ class CreateOrganizationTables extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('organization_tables');
+        Schema::dropIfExists('organization_user');
+        Schema::dropIfExists('organization_user_levels');
+        Schema::dropIfExists('organizations');
+        Schema::dropIfExists('organization_categories');
+        Schema::dropIfExists('organization_types');
     }
 }
