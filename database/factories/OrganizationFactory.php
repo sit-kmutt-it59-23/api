5b85f0be3d8ae5972f9f3d303577a3a638b2c177
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2019-05-17 21:57:23 +0700

    [CREATE] Make a factory for Organization's model.

---
+++ database/factories/OrganizationFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use Carbon\Carbon;
+use Faker\Factory as FakerFactory;
+use Faker\Generator as Faker;
+
+use App\Models\Organization;
+use App\Models\OrganizationCategory;
+use App\Models\OrganizationType;
+
+$fakerTh = FakerFactory::create("th_TH");
+
+$factory->define(Organization::class, function (Faker $faker) use ($fakerTh) {
+    return [
+        'name' => $fakerTh->company,
+        'name_en' => $faker->company,
+        'description' => $faker->realText('255'),
+        'slogan' => $faker->catchPhrase,
+        'allowed_at' => $faker->dateTimeBetween('-1 month', 'now'),
+        'expired_at' => $faker->dateTimeBetween('-1 months', '1 months')
+    ];
+});
+
+$factory->state(Organization::class, 'student_club', [
+    'type_id' => OrganizationType::where('name', 'student_club')->first()->id,
+    'category_id' => function () {
+        return OrganizationCategory::inRandomOrder()->first()->id;
+    }
+]);
