0603f0db5e6739eb42121cff79b46a248ad838ec
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2019-05-19 15:48:08 +0700

    [UPDATE] Change pivot model to standard model for DocumentProject.

--- app/Models/DocumentProject.php
+++ app/Models/DocumentProject.php
@@ -1,14 +1,10 @@
 <?php
-
 /**
  * Created by Reliese Model.
  * Date: Sat, 11 May 2019 14:50:03 +0700.
  */
-
 namespace App\Models;
-
-use Illuminate\Database\Eloquent\Relations\Pivot as Eloquent;
-
+use Illuminate\Database\Eloquent\Model as Eloquent;
 /**
  * Class DocumentProject
  * 
@@ -23,25 +19,23 @@ use Illuminate\Database\Eloquent\Relations\Pivot as Eloquent;
  * @property \Carbon\Carbon $created_at
  * @property \Carbon\Carbon $updated_at
  * 
+ * @property \App\Models\DocumentProjectCategory $document_project_category
+ * @property \App\Models\Organization $organization
  * @property \Illuminate\Database\Eloquent\Collection $documents
  *
  * @package App\Models
  */
 class DocumentProject extends Eloquent
 {
-	protected $increments = true;
-
 	protected $casts = [
 		'organization_id' => 'int',
 		'category_id' => 'int',
 		'budget_amount' => 'float'
 	];
-
 	protected $dates = [
 		'start_at',
 		'end_at'
 	];
-
 	protected $fillable = [
 		'organization_id',
 		'category_id',
@@ -51,6 +45,16 @@ class DocumentProject extends Eloquent
 		'start_at',
 		'end_at'
 	];
+
+	public function document_project_category()
+	{
+		return $this->belongsTo(\App\Models\DocumentProjectCategory::class, 'category_id');
+	}
+
+	public function organization()
+	{
+		return $this->belongsTo(\App\Models\Organization::class);
+	}
 
 	public function documents()
 	{
