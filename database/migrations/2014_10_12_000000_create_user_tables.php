1244ec7e6bd4da0d8867b997b96e12291bbd2db7
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2019-06-07 22:59:11 +0700

    [UPDATE] Add some column for personal information in 'user_data'.

--- database/migrations/2014_10_12_000000_create_user_tables.php
+++ database/migrations/2014_10_12_000000_create_user_tables.php
@@ -33,8 +33,24 @@ class CreateUserTables extends Migration
             $table->string('first_name', 64);
             $table->string('middle_name', 64)->nullable();
             $table->string('last_name', 64);
+            $table->string('first_name_th', 64)->nullable();
+            $table->string('middle_name_th', 64)->nullable();
+            $table->string('last_name_th', 64)->nullable();
             $table->string('student_id', 11)->nullable()->unique();
+            $table->string('study_major_code')->nullable();
+            $table->decimal('score_gpa', 4, 2)->unsigned()->nullable();
+            $table->string('activity_experience')->nullable();
+            $table->string('addr_street_1')->nullable();
+            $table->string('addr_street_2')->nullable();
+            $table->string('addr_sub_district', 64);
+            $table->string('addr_district', 64);
+            $table->string('addr_state', 64);
+            $table->string('addr_postal_code', 16);
+            $table->string('addr_country', 64)->nullable();
             $table->string('tel_no', 15)->nullable();
+            $table->string('email');
+            $table->string('image_path_official')->nullable();
+            $table->string('image_path_profile')->nullable();
             $table->datetime('created_at')->useCurrent();
             $table->datetime('updated_at')
                 ->default(
